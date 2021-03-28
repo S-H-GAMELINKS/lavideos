68c1fe075e77dc7365be3cce10c9a1beca4755f1
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-03-28 17:32:18 +0900

    add videos default layout

---
+++ resources/views/layouts/video.blade.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <link href="https://vjs.zencdn.net/7.11.4/video-js.css" rel="stylesheet" />
+    </head>
+    <body>
+        @yield('content')
+        <script src="https://vjs.zencdn.net/7.11.4/video.min.js"></script>
+    </body>
+</html>
